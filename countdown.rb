e9268e95dc8d501be2a947f6d3c05abab45c77aa
Tara Sabella <tarasabella@gmail.com>
2020-11-13 18:46:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
-end
+def countdown(number)
+  number = 10
+  while number >= 1 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+ end
+ return "HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep(number)
+  sleep(5.seconds)
+
+  number = 10 
+  while number >= 1 
+  puts "#{number} SECOND(S)!"
+  number -= 1
+ end
+ return "HAPPY NEW YEAR!"
+end 
+  
